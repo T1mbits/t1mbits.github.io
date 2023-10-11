39981d6faac4a9f04fbb95c2c40de71992051853
T1mbits <timbits1123@gmail.com>
2023-10-11 17:56:02 -0400

    Removed double quotes

--- js/home.js
+++ js/home.js
@@ -2,34 +2,34 @@ var activePage = 0;
 
 $(document).ready(function () {
 	// Page Initialization
-	$("#about").hide();
-	$("#filler").hide();
+	$('#about').hide();
+	$('#filler').hide();
 	// Header Stuff
-	$(".header_button").click(function () {
+	$('.header_button').click(function () {
 		// changing active button id (cosmetic)
-		$("#header_active").removeAttr("id");
-		$(this).prop("id", "header_active");
+		$('#header_active').removeAttr('id');
+		$(this).prop('id', 'header_active');
 		// changing page according to button
-		switch ($(this).attr("class")) {
-			case "header_button page_0":
-				$("#home").show();
-				$("#about").hide();
-				$("#filler").hide();
+		switch ($(this).attr('class')) {
+			case 'header_button page_0':
+				$('#home').show();
+				$('#about').hide();
+				$('#filler').hide();
 				break;
-			case "header_button page_1":
-				$("#home").hide();
-				$("#about").show();
-				$("#filler").hide();
+			case 'header_button page_1':
+				$('#home').hide();
+				$('#about').show();
+				$('#filler').hide();
 				break;
-			case "header_button page_2":
-				$("#home").hide();
-				$("#about").hide();
-				$("#filler").show();
+			case 'header_button page_2':
+				$('#home').hide();
+				$('#about').hide();
+				$('#filler').show();
 				break;
 			default:
-				$("#home").show();
-				$("#about").hide();
-				$("#filler").hide();
+				$('#home').show();
+				$('#about').hide();
+				$('#filler').hide();
 				break;
 		}
 	});
